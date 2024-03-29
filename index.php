5a8e2efb98029305d1b59cbb55def53a360c086c
ayedi <islem.ayedi@insat.ucar.tn>
2024-03-30 00:59:28 +0100

    first push

--- index.php
+++ index.php
@@ -1,13 +1,240 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="indexStyle.css">
 </head>
 <body>
-        <h1>php project</h1>
-        <p>i made this change and its recording it yeeey</p>
+    <header>
+        <div class="logo">
+            <img src="src/logoblack-removebg.png" alt="Company Logo">
+                
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#AboutUs">About Us</a></li>
+                <li><a href="#Books">Books</a></li>
+            </ul>
+        </nav>
+        <div class="user-actions">
+            <a href="cart.php">Cart</a>
+            <a href="login.php">Login</a>
+        </div>
+    </header>
+
+
+    <section id="AboutUs">
+        <div class="container">
+    <h1 id="titleAboutUs">About Us</h1>
+    <p>Welcome to <span class="shop-name">Book Shop</span>, where our passion for literature meets your reading needs.</p>
+    
+    <p>At <span class="shop-name">Book Shop</span>, we believe that books have the power to inspire, educate, and entertain. With a carefully curated selection of titles spanning various genres, we strive to cater to every reader's taste and preference. Whether you're a lifelong bibliophile or just starting your reading journey, we have something for everyone.</p>
+    
+    <p>Our team consists of avid readers who are dedicated to providing exceptional service and helping you discover your next favorite book. From bestsellers to hidden gems, we're here to guide you through our shelves and recommend the perfect read for any occasion.</p>
+    
+    <p>But <span class="shop-name">Book Shop</span> is more than just a bookstore. We're a community hub where book lovers can come together to share their love of reading. Join us for author events, book clubs, and other literary gatherings that celebrate the written word and foster a sense of connection among readers.</p>
+    
+    <p>Thank you for choosing <span class="shop-name">Book Shop</span> as your literary destination. We look forward to serving you and being a part of your reading adventures.</p>
+</div>
+    </section>
+    <section id="Books">
+    <div class="grid-container">
+        <div class="grid-item">
+                <img src="src/atomichabits.jpg"  id="image">
+                <p class="title" >Atomic Habits</p>
+                <div class="rating1">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description">Atomic Habits (EXP): An Easy & Proven Way to Build Good Habits & Break Bad Ones Paperback – Édition internationale, 1 octobre 2019</p>
+                 <div><span class="price">19.47£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+        <div class="grid-item">
+                <img src="src/normalpeople.jpg"  id="image">
+                <p class="title" >normal people</p>
+                <div class="rating2">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description">Normal People: One million copies sold and turned into a netflix series</p>
+                <div><span class="price">12.29£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
         
+        <div class="grid-item">
+                <img src="src/prideprejudice.jpg"  id="image">
+                <p class="title" >pride and prejudice </p>
+                <div class="rating3">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description">Pride and Prejudice: The Complete Novel, with Nineteen Letters from the Characters' Correspondence, Written and Folded by Hand </p>
+                <div><span class="price">32.36£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+        
+
+       
+        <div class="grid-item">
+                <img src="src/oneday.jpg"  id="image">
+                <p class="title" >one day  </p>
+                <div class="rating4">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="stars">&#9733;</span>
+                </div>
+                <p class="description">One Day: Now a major Netflix series  </p>
+                <div><span class="price">11.05£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+        <div class="grid-item">
+                <img src="src/divergent.jpg"  id="image">
+                <p class="title" >divetrgent series</p>
+                <div class="rating5">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description"> [Divergent / Insurgent / Allegiant / Four] [By: Roth, Veronica] [July, 2014] </p>
+                <div><span class="price">140.09£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+
+        <div class="grid-item">
+                <img src="src/secretgarden.jpg"  id="image">
+                <p class="title" >secret garden </p>
+                <div class="rating6">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="star">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description"> THE SECRET GARDEN By Frances Hodgson Burnett, ILLUSTRATED BY MB Kork </p>
+                <div><span class="price">11.5£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+
+        <div class="grid-item">
+                <img src="src/metamo.jpg"  id="image">
+                <p class="title" >The Metamorphosis </p>
+                <div class="rating7">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="stars">&#9733;</span>
+                </div>
+                <p class="description"> The Metamorphosis and Other Stories (Oxford World's Classics) (English Edition) </p>
+                <div><span class="price">6.15£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+        <div class="grid-item">
+                <img src="src/kiterunner.jpg"  id="image">
+                <p class="title" >The Kite Runner </p>
+                <div class="rating8">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description"> a world class tragedy by Khaled Housseini </p>
+               <div><span class="price">12.29£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+        <div class="grid-item">
+                <img src="src/awoman.jpg"  id="image">
+                <p class="title" >A Woman is No Man</p>
+                <div class="rating9">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description"> PRE-ORDER ETAF RUM’S NEW NOVEL, EVIL EYE, NOW – COMING SEPTEMBER 2024.</p>
+                <div><span class="price">11.05£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+
+        <div class="grid-item">
+                <img src="src/100.jpg"  id="image">
+                <p class="title" >One Hundred Years of Solitude</p>
+                <div class="rating10">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="star">&#9733;</span>
+                </div>
+                <p class="description">One Hundred Years of Solitude by Gabriel García Marquez </p>
+                <div><span class="price">15.3£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+
+        <div class="grid-item">
+                <img src="src/athousand.jpg"  id="image">
+                <p class="title" > A Thousand Splendid Suns</p>
+                <div class="rating11">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="stars">&#9733;</span>
+                </div>
+                <p class="description"> A Thousand Splendid Suns and The New York Times: Middle Eastern Women </p>
+                <div><span class="price">12.8£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+
+        <div class="grid-item">
+                <img src="src/wuthering.jpg"  id="image">
+                <p class="title" >Wuthering Heights</p>
+                <div class="rating12">
+                <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span  class="stars">&#9733;</span>
+                   <span class="stars">&#9733;</span>
+                    <span class="stars">&#9733;</span>
+                </div>
+                <p class="description">Wuthering Heights: [Annotated Edition – Bonus: Emily Brontë's Biography and Historical Context] </p>
+                <div><span class="price">9.48£</span> <button class="button">add to cart</button></div>
+
+
+        </div>
+    </div>
+    </section>
+
 </body>
 </html>
