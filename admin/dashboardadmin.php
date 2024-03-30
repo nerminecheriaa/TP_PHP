d912fa405ab848b4f19869d4873651aeb5cf474a
ayedi <islem.ayedi@insat.ucar.tn>
2024-03-30 02:22:08 +0100

    dashboard add

---
+++ admin/dashboardadmin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="dashboardadmin.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../src/logoblack-removebg.png" alt="Company Logo">
+                
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../index.php">home</a></li>
+            </ul>
+        </nav>
+        <div class="user-actions">
+            <a href="../login.php">Login</a>
+        </div>
+    </header>
+</body>
+</html>
